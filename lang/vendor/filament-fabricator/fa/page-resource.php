18aaf4debd3a527398f9d1e86602c25645478c98
mahdi-nasirlo <m.nwordpress1223@gmail.com>
2022-10-25 23:17:35 +0330

    add page builder

---
+++ lang/vendor/filament-fabricator/fa/page-resource.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'labels' => [
+        'blocks' => 'Blocks',
+        'title' => 'Title',
+        'slug' => 'Slug',
+        'layout' => 'Layout',
+        'parent' => 'Parent',
+        'url' => 'URL',
+    ],
+
+    'actions' => [
+        'visit' => 'Visit',
+    ],
+];
