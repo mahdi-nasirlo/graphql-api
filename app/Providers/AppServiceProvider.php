75e0aca2757fe3c7f9c861fbcc8fa4041f5219dc
mahdi-nasirlo <m.nwordpress1223@gmail.com>
2022-10-25 21:31:57 +0330

    change font for admin

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Filament\Facades\Filament;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,5 +24,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Filament::serving(
+            function () {
+                Filament::registerStyles([
+                    '/resources/css/font.css',
+                    asset('css/admin-font.css'),
+                ]);
+            }
+        );
     }
 }
